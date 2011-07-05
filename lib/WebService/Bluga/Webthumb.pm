08dfdfb348918998dc638608a17719e0897a8833
David Precious <davidp@preshweb.co.uk>
2011-07-05 23:36:48 +0100

    Basic initial version.

---
+++ lib/WebService/Bluga/Webthumb.pm
@@ -0,0 +1,110 @@
+package WebService::Bluga::Webthumb;
+
+use warnings;
+use strict;
+
+=head1 NAME
+
+WebService::Bluga::Webthumb - The great new WebService::Bluga::Webthumb!
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use WebService::Bluga::Webthumb;
+
+    my $foo = WebService::Bluga::Webthumb->new();
+    ...
+
+=head1 EXPORT
+
+A list of functions that can be exported.  You can delete this section
+if you don't export anything, such as for a purely object-oriented module.
+
+=head1 SUBROUTINES/METHODS
+
+=head2 function1
+
+=cut
+
+sub function1 {
+}
+
+=head2 function2
+
+=cut
+
+sub function2 {
+}
+
+=head1 AUTHOR
+
+David Precious, C<< <davidp at preshweb.co.uk> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-webservice-bluga-webthumb at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=WebService-Bluga-Webthumb>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc WebService::Bluga::Webthumb
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=WebService-Bluga-Webthumb>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/WebService-Bluga-Webthumb>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/WebService-Bluga-Webthumb>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/WebService-Bluga-Webthumb/>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2011 David Precious.
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of either: the GNU General Public License as published
+by the Free Software Foundation; or the Artistic License.
+
+See http://dev.perl.org/licenses/ for more information.
+
+
+=cut
+
+1; # End of WebService::Bluga::Webthumb
